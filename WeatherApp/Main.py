826f30cddd638842562d3ba3571e659f04c58a46
MD AL AMIN SARKER <alamin.srk@gmail>
2022-11-18 00:15:51 +0930

    updated info for goup info

---
+++ WeatherApp/Main.py
@@ -0,0 +1,20 @@
+""" HIT137 Assignment 2 Group 74 """
+####
+# Group 74
+
+# STUDENT NAME: MD AL AMIN SARKER
+# STUDENT NUMBER: S361035
+
+# STUDENT NAME: SHIVA GUPTA
+# STUDENT NUMBER: S361033
+
+# Youtube link: https://youtu.be/EQCzxKP5src
+####
+
+
+from DailyWeather import DailyWeather
+
+
+# Run the Daily Weather App
+if __name__ == "__main__":
+    d = DailyWeather()
